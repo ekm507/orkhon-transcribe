0ba793c6a817af57c32e1cf2c47d9b8a9631608c
Erfan Kheyrollahi <ekm507@gmail.com>
2023-05-19 02:09:09 +0330

    add map function. basic error handling

--- orkhon-transcribe.py
+++ orkhon-transcribe.py
@@ -1,3 +1,9 @@
 latin_glyphs =  'qwertyuiopasdfghjklzxcvbnmüışğäİö'
 orkhon_glyphs = '𐰴𐰙𐰅𐰺𐱃𐰖𐰆𐰄𐰗𐰯𐰀𐰽𐰑𐰊𐰏𐰎𐰳𐰚𐰞𐰔𐰐𐰕𐰋𐰉𐰣𐰢𐰈𐰃𐱁𐰍𐰂𐰄𐰘'
 latin_to_orkhon_map = {latin_glyphs[i] : orkhon_glyphs[i] for i in range(len(latin_glyphs))}
+def latin_letter_to_orkhon(letter:str) -> str:
+    if letter in latin_to_orkhon_map.keys():
+        orkhon_letter = latin_to_orkhon_map[letter]
+    else:
+        orkhon_letter = letter
+    return orkhon_letter
