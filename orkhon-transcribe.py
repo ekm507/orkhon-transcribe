40731b8f4e30d15623cfb52f32ef01aaaef85e27
Erfan Kheyrollahi <ekm507@gmail.com>
2023-05-19 02:09:43 +0330

    add translate function. get from argv

--- orkhon-transcribe.py
+++ orkhon-transcribe.py
@@ -1,3 +1,5 @@
+from sys import argv
+
 latin_glyphs =  'qwertyuiopasdfghjklzxcvbnmüışğäİö'
 orkhon_glyphs = '𐰴𐰙𐰅𐰺𐱃𐰖𐰆𐰄𐰗𐰯𐰀𐰽𐰑𐰊𐰏𐰎𐰳𐰚𐰞𐰔𐰐𐰕𐰋𐰉𐰣𐰢𐰈𐰃𐱁𐰍𐰂𐰄𐰘'
 latin_to_orkhon_map = {latin_glyphs[i] : orkhon_glyphs[i] for i in range(len(latin_glyphs))}
@@ -7,3 +9,7 @@ def latin_letter_to_orkhon(letter:str) -> str:
     else:
         orkhon_letter = letter
     return orkhon_letter
+
+word = ' '.join(argv[1:])
+newword = ''.join(map(latin_letter_to_orkhon, word))
+print(newword)
