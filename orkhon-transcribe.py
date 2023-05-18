f015d3eda9b5f08ff4cb2c54ce4472c345f12fe1
Erfan Kheyrollahi <ekm507@gmail.com>
2023-05-19 02:08:43 +0330

    add latin to orkhon map

--- orkhon-transcribe.py
+++ orkhon-transcribe.py
@@ -1,2 +1,3 @@
 latin_glyphs =  'qwertyuiopasdfghjklzxcvbnmüışğäİö'
 orkhon_glyphs = '𐰴𐰙𐰅𐰺𐱃𐰖𐰆𐰄𐰗𐰯𐰀𐰽𐰑𐰊𐰏𐰎𐰳𐰚𐰞𐰔𐰐𐰕𐰋𐰉𐰣𐰢𐰈𐰃𐱁𐰍𐰂𐰄𐰘'
+latin_to_orkhon_map = {latin_glyphs[i] : orkhon_glyphs[i] for i in range(len(latin_glyphs))}
